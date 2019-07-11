6faca9a702761fbd0be50bc3870dc35cf8e0b3d2
manuellam31 <52497881+manuellam31@users.noreply.github.com>
2019-07-11 18:45:02 -0300

    Update ESI Optimization.R

--- ESI Optimization.R
+++ ESI Optimization.R
@@ -3,7 +3,7 @@
 library(rsm)
 library(Vdgraph)
 
-#Optimzing temperature, esi voltage and curtain gas
+#Optimazing temperature, esi voltage and curtain gas
 bbdmodel<-bbd(y~x1+x2+x3,block = FALSE,n0 = 3, coding=list(x1~(TEM-600)/100, x2~(ESIV-4500)/500, x3~(CUR-20)/10),randomize=FALSE)
 head(bbdmodel)
 Vdgraph(bbdmodel[ , 3:5]) #variance dispersion graph
